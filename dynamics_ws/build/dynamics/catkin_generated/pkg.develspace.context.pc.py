86b63fa9ea2d7aca8f9910674f4abc993a6cf43d
Robopunk <1042726221@qq.com>
2023-07-18 19:24:00 +0800

    delete build

--- dynamics_ws/build/dynamics/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/crp/git/dynamics_ws/src/dynamics/include".split(';') if "/home/crp/git/dynamics_ws/src/dynamics/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ldynamics".split(';') if "-ldynamics" != "" else []
-PROJECT_NAME = "dynamics"
-PROJECT_SPACE_DIR = "/home/crp/git/dynamics_ws/devel"
-PROJECT_VERSION = "0.0.0"
